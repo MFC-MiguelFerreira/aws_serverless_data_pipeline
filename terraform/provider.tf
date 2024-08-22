a86bc56ac60e61628d209c39d268a7261c8c2256
MFC-MiguelFerreira <mfc.miguelferreira@gmail.com>
2024-08-21 23:02:45 -0300

    🧱 terraform two new variable: environment and cost tracker

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
   profile = var.aws_profile_name
-  region = var.aws_region
+  region  = var.aws_region
+  default_tags {
+    tags = {
+      "environment" : var.environment
+      "cost_tracker" : var.infrastucture_tag
+    }
+  }
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,24 @@
 variable "aws_profile_name" {
-  type = string
+  type        = string
   description = "AWS profile locally configured with aws configure or aws configure sso command."
-  nullable = false
+  nullable    = false
 }
 
 variable "aws_region" {
-  type = string
+  type        = string
   description = "AWS region to deploy the infrastructure."
-  nullable = false
+  nullable    = false
+}
+
+variable "environment" {
+  type        = string
+  description = "The development environment (e.g. dev/prd)"
+  nullable    = false
+  default     = "dev"
+}
+
+variable "infrastucture_tag" {
+  type        = string
+  description = "Cost tracker tag (e.g. terraform_serverless_data_pipeline)."
+  nullable    = true
 }
