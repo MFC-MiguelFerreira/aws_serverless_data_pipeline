c98c0e061b6c080cd6125f0dd2ff6a65737b1033
MFC-MiguelFerreira <mfc.miguelferreira@gmail.com>
2024-09-21 12:43:03 -0300

    🧱 ecr trigger to update the latest lambda image and "deploy" the code changes

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,14 @@
+locals {
+  source_path   = "../"
+  path_include  = ["**/lambdas/**", "**/sql/**", "**/pipe/**"]
+  path_exclude  = ["**/__pycache__/**"]
+  files_include = setunion([for f in local.path_include : fileset(local.source_path, f)]...)
+  files_exclude = setunion([for f in local.path_exclude : fileset(local.source_path, f)]...)
+  files         = sort(setsubtract(local.files_include, local.files_exclude))
+
+  dir_sha = sha1(join("", [for f in local.files : filesha1("${local.source_path}/${f}")]))
+}
+
 module "docker_image" {
   source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
@@ -5,15 +16,20 @@ module "docker_image" {
 
   use_image_tag = false
 
-  source_path = "../"
+  source_path = local.source_path
   platform    = "linux/amd64"
+
+  triggers = {
+    dir_sha = local.dir_sha
+  }
 }
 
 module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name         = "${var.infrastucture_prefix_name}"
-  repository_force_delete = true
+  repository_name                 = var.infrastucture_prefix_name
+  repository_force_delete         = true
+  repository_image_tag_mutability = "MUTABLE"
 
   create_lifecycle_policy = false
 
